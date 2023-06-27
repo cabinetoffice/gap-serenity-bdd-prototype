1f746367018ec786ffe075a76bd0fc7bb1350fb1
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2023-06-27 09:27:24 +0100

    updating test cases

--- src/test/java/starter/submissions/SubmissionActions.java
+++ src/test/java/starter/submissions/SubmissionActions.java
@@ -9,19 +9,19 @@ import org.hamcrest.Matchers;
 
 public class SubmissionActions extends UIInteractions {
 
-    @Given("Given the /submissions/{0} endpoint")
+    @Given("An application form with ID 1 exists in the system")
     public void givenTheSubmissionsApiEndpoint() {
         given()
             .baseUri("http://prototype:8080")
             .basePath("/submissions");
     }
 
-    @When("When the endpoint is called with ID 1")
+    @When("The `/submissions/1`endpoint is called")
     public void whenTheEndpointIsCalledWithValidId() {
         when().get("/1");
     }
 
-    @Then("Then The API returns a submission")
+    @Then("The API fetches all the submissions for this application form")
     public void thenResponseStatusShouldBeOk() {
         then().body("submissions[0].submissionId", Matchers.equalTo("3a6cfe2d-bf58-440d-9e07-3579c7dcf207"));
     }
